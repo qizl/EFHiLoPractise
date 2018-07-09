4e3848fd842180d04c9b8b3527df9c96ed043059
qizl <q@qizl.cn>
2018-07-09 11:40:28 +0800

    create pjt

---
+++ src/EFHiLoPractise/Models/HiLoContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFHiLoPractise.Models
+{
+    public class HiLoContext : DbContext
+    {
+        public HiLoContext(DbContextOptions<HiLoContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<VipUser> VipUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(ConfigUser);
+            builder.Entity<VipUser>(ConfigVipUsers);
+        }
+
+        public void ConfigUser(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.Property(m => m.ID).ForSqlServerUseSequenceHiLo("sequence_test").IsRequired();
+        }
+
+        public void ConfigVipUsers(EntityTypeBuilder<VipUser> builder)
+        {
+            builder.ToTable("VipUsers");
+            builder.Property(m => m.ID).ForSqlServerUseSequenceHiLo("sequence_test").IsRequired();
+        }
+    }
+}
---
+++ src/EFHiLoPractise/Models/VipUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EFHiLoPractise.Models
+{
+    public class VipUser
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
